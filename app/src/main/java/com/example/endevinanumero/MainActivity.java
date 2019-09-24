1263f077b7a263c75e9450499f973141d2259c52
adriangc24 <adriangcamacho24@gmail.com>
2019-09-24 17:53:00 +0200

    App acabada

--- app/src/main/java/com/example/endevinanumero/MainActivity.java
+++ app/src/main/java/com/example/endevinanumero/MainActivity.java
@@ -15,12 +15,14 @@ public class MainActivity extends AppCompatActivity {
     EditText et1;
     TextView tv1;
     public int numRandomMethod(int numRandom){
-        return numRandom=(int)(Math.random()*100);
+         numRandom=(int)(Math.random()*100);
+        return numRandom;
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        numRandom=numRandomMethod(0);
 
         et1=(EditText)findViewById(R.id.editText);
         tv1=(TextView)findViewById(R.id.textView);
@@ -29,19 +31,17 @@ public class MainActivity extends AppCompatActivity {
     public void button (View view){
         if(Integer.valueOf(et1.getText().toString())==numRandom){
             tv1.setText("Intentos: "+intentos++);
-            Toast.makeText(this,"NUMERO TROBAT !",Toast.LENGTH_LONG).show();
+            tv1.setText("Felicitats has guanyat en "+intentos+" intents");
             numRandom=numRandomMethod(0);
             intentos=0;
-            tv1.setText("Intentos: "+intentos++);
-
         }
 
-         if(Integer.valueOf(et1.getText().toString())<numRandom){
+         else if(Integer.valueOf(et1.getText().toString())<numRandom){
             Toast.makeText(this,"El numero introduit es mes petit",Toast.LENGTH_LONG).show();
             tv1.setText("Intentos: "+intentos++);
 
         }
-         if(Integer.valueOf(et1.getText().toString())>numRandom){
+         else if(Integer.valueOf(et1.getText().toString())>numRandom){
             Toast.makeText(this,"El numero introduit es mes gran",Toast.LENGTH_LONG).show();
             tv1.setText("Intentos: "+intentos++);
 
