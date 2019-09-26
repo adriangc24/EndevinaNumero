08961383cc7008e47de8881feeff5bf91e10817d
adriangc24 <adriangcamacho24@gmail.com>
2019-09-26 19:25:32 +0200

    colour green of text when you win

--- app/src/main/java/com/example/endevinanumero/MainActivity.java
+++ app/src/main/java/com/example/endevinanumero/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.endevinanumero;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     int intentos=1;
     EditText et1;
     TextView tv1;
+    Button b1;
     public int numRandomMethod(int numRandom){
          numRandom=(int)(Math.random()*100);
         return numRandom;
@@ -27,9 +30,11 @@ public class MainActivity extends AppCompatActivity {
 
         et1=(EditText)findViewById(R.id.editText);
         tv1=(TextView)findViewById(R.id.textView);
+        b1 = (Button)findViewById(R.id.button);
     }
 
     public void button (View view){
+        b1.setText("Guardar");
         tv1.setTextColor(Color.BLACK);
         tv1.setTextSize(18);
         if(et1.getText().toString().isEmpty()){
@@ -38,11 +43,13 @@ public class MainActivity extends AppCompatActivity {
 
         else if(Integer.valueOf(et1.getText().toString())==numRandom){
             tv1.setText("Intentos: "+intentos++);
+            b1.setText("Reiniciar");
             tv1.setText("Felicitats has guanyat en "+intentos+" intents");
             tv1.setTextColor(Color.GREEN);
             tv1.setTextSize(24);
             numRandom=numRandomMethod(0);
             intentos=0;
+
         }
 
          else if(Integer.valueOf(et1.getText().toString())<numRandom){
