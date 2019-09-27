56738852fe79ef64ef323515f2cc76eba96790f8
adriangc24 <adriangc24@users.noreply.github.com>
2019-09-27 11:33:13 +0200

    firsat auth with firebase

--- app/src/main/java/com/example/endevinanumero/LoginActivity.java
+++ app/src/main/java/com/example/endevinanumero/LoginActivity.java
@@ -3,12 +3,40 @@ package com.example.endevinanumero;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText etEmail, etPass;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // ...
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        etEmail = (EditText)findViewById(R.id.editText3);
+        etPass = (EditText)findViewById(R.id.editText2);
+
+
+            }
+    public void auth(View view) {
+        // Check if user is signed in (non-null) and update UI accordingly.
+        if(!etEmail.getText().toString().isEmpty()&&!etPass.getText().toString().isEmpty()) {
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+        }
+        else {
+            Toast.makeText(this,"ERROR: 1 o mas campos vacíos",Toast.LENGTH_LONG).show();
+        }
+
+
     }
+
 }
